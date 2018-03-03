90f8242bf5e5633e5ee092da6ec329a3f2be0af9
NikolenkoOlga <nikolenkoolga13@gmail.com>
2018-03-03 14:04:18 +0300

    commit 1

---
+++ Task_1/MaxSequence.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Task_1
+{
+    /// <summary>
+    ///  This class contains the method for counting the maximum number of identical consecutive elements in a string
+    /// </summary>
+    class MaxSequence
+    {
+        public string inputString;
+        public MaxSequence(string maxSequence)
+        {
+            inputString = maxSequence;
+        }
+
+        /// <summary>
+        /// Method for counting the maximum number of identical consecutive elements in a string
+        /// </summary>
+        /// <param name="str">string for processing</param>
+        /// <returns> The maximum number of identical consecutive elements in a string </returns>
+        public int CalculateMaxNumber()
+        {
+
+            int counterOfChars = 1;
+            int maxSequence = 0;
+            for (int i = 0; i < inputString.Length - 1; i++)
+            {
+                if (inputString[i] == inputString[i + 1])
+                {
+                    counterOfChars++;
+                }
+                else
+                {
+                    if (counterOfChars > maxSequence)
+                    {
+                        maxSequence = counterOfChars;
+                    }
+                    counterOfChars = 1;
+                }
+            }
+            if (counterOfChars > maxSequence)
+            {
+                maxSequence = counterOfChars;
+            }
+            return maxSequence;
+        } 
+     }
+}
+
+
+
+   
---
+++ Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Entry
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                MaxSequence maxSequense = new MaxSequence(args[0]);
+                int maxCount = maxSequense.CalculateMaxNumber();
+                Console.WriteLine("Maximum sequence of chars in string = {0}", maxCount);
+            }
+            else
+            {
+                Console.WriteLine("The string wasn't entered");
+            }
+        }
+        
+    }
+}
