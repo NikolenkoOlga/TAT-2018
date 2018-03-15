1ec5ab5e4426525cb7eb50e1f2bbd31c744a313f
NikolenkoOlga <nikolenkoolga13@gmail.com>
2018-03-15 23:44:48 +0300

    commit 1 and 3

---
+++ Task_1/MaxSequence.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task_1
+{
+    /// <summary>
+    ///  This class contains the method for counting the maximum number of identical consecutive elements in a string
+    /// </summary>
+    class MaxSequence
+    {
+        public string inputString;
+        public MaxSequence(string maxSequence)
+        {
+            inputString = maxSequence;
+        }
+        /// <summary>
+        /// Method for counting the maximum number of identical consecutive elements in a string
+        /// </summary>
+        /// <returns> The maximum number of identical consecutive elements in a string </returns>
+        public int CalculateMaxNumber()
+        {
+            int currentCount = 1;
+            int maxCount = currentCount;
+            for (int i = 1; i < inputString.Length; i++)
+            {
+                if (inputString[i - 1] == inputString[i])
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    if (currentCount > maxCount)
+                    {
+                        maxCount = currentCount;
+                    }
+                    currentCount = 1;
+                }
+            }
+            if (currentCount > maxCount)
+            {
+                maxCount = currentCount;
+            }
+            return maxCount;
+        } 
+     }
+}
