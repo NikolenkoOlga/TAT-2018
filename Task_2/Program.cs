015114f313eba0188f1e9d394ce778809b65be0b
NikolenkoOlga <nikolenkoolga13@gmail.com>
2018-03-16 16:15:51 +0300

    xUnit Test

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -7,8 +7,10 @@ namespace Task_2
     {
         static void Main()
         {
-            OutputEvenIndex inputString = new OutputEvenIndex(Console.ReadLine());
-            inputString.FindEvenIndex();   
+            string inputString = Console.ReadLine();
+            OutputEvenIndex outputString = new OutputEvenIndex(inputString);
+            Console.WriteLine(outputString.DeleteAndPrintEvenIndex());
+            Console.ReadKey();
         }
     }
 }
---
+++ xUnitTestProject2/Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using Xunit;
+using Task_2;
+
+
+
+namespace xUnitTestProject2
+{
+    public class Test
+    {
+        [Fact]
+        public void EmptyStringTest()
+        {
+            //act
+            OutputEvenIndex inputString = new OutputEvenIndex(String.Empty);
+            //assert
+            Assert.Null(inputString.DeleteAndPrintEvenIndex());
+        }
+
+        [Fact]
+        public void EvenIndexTest()
+        {
+            //arrange
+            string str = "rjnbrb";
+            string result = "rnr";            
+            //act
+            OutputEvenIndex inputString = new OutputEvenIndex(str);
+            //assert
+            Assert.Equal(result, inputString.DeleteAndPrintEvenIndex());
+        }
+
+        [Fact]
+        public void EvenIndexWithSymbolsTest()
+        {
+            //arrange
+            string str = "r*2**jn// b _R _bBR_' ";
+            string result = "r2*n/b_ bR'";
+            //act
+            OutputEvenIndex inputString = new OutputEvenIndex(str);
+            //assert
+            Assert.Equal(result, inputString.DeleteAndPrintEvenIndex());
+        }
+    }
+}
